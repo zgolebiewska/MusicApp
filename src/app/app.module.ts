5ccba302a05533ea792898ae3743fb19893e3e59
zgolebiewska <zgolebiewska5@gmail.com>
2023-02-07 18:43:44 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComposersListComponent } from './api/composers/composers-list/composers-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposersListComponent
   ],
   imports: [
     BrowserModule
